a623ab5e92cbed96a1a6ae8d751f5bbbc1891d34
Pow Ehrmantrut <pedrohfdias.15122007@gmail.com>
2026-02-22 18:23:59 -0300

    ae

--- bot.py
+++ bot.py
@@ -226,9 +226,15 @@ class BanModal(discord.ui.Modal, title="Banir Membro"):
     async def on_submit(self, interaction: discord.Interaction):
 
         embed_dm = discord.Embed(
-            title="Você foi banido do servidor\n\n",
+            title="🚫 Você foi banido do servidor",
             description=f"**Motivo:**\n{self.motivo.value}",
             color=discord.Color.red()
+        )
+
+        embed_dm.set_thumbnail(url=self.membro.display_avatar.url)
+
+        embed_dm.set_image(
+            url="https://cdn.discordapp.com/attachments/1444735189765849320/1474956398235353108/Logo_SSP.png?ex=699c6470&is=699b12f0&hm=5cc66b9a477a160abe7a92d291683d0dafde0659a13f868950ecd0f3be25fd9d&"
         )
         
 
